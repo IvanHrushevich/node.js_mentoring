d8ba50afe8072d1aeb2a493c6b68a9f7c169a2ac
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2019-12-06 17:08:27 +0300

    Add ignoring of "Amount" column in csv file

--- src/task2.js
+++ src/task2.js
@@ -8,12 +8,10 @@ const pipeline = util.promisify(stream.pipeline);
 const csvFilePath = './src/csv/task-2-table.csv';
 const txtFilePath = './src/task-2-table.txt';
 
-async function convertCsvToJson() {
-  await pipeline(
-    fs.createReadStream(csvFilePath),
-    csv(),
-    fs.createWriteStream(txtFilePath)
-  );
-}
-
-convertCsvToJson().catch(err => console.error('error: ', err));
+pipeline(
+  fs.createReadStream(csvFilePath),
+  csv({
+    ignoreColumns: /Amount/
+  }),
+  fs.createWriteStream(txtFilePath)
+).catch(err => console.error('error: ', err));
