b91dce241f06f3f9918a80d1fd3ade5a3d5e986d
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2019-12-20 03:43:28 +0300

    Add basic post middleware

--- src/hw2/app.js
+++ src/hw2/app.js
@@ -2,12 +2,21 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
-const useersRouter = express.Router();
 
 const users = {};
 
 app.listen(port, () => console.log(`Users app listening on port ${port}!`));
 
+const useersRouter = express.Router();
+useersRouter.use(express.json());
+
 useersRouter.get('/', (req, res) => res.json(users));
+
+useersRouter.post('/', (req, res) => {
+  const user = req.body;
+  console.log('user', user);
+  users[user.id] = user;
+  res.sendStatus(200);
+});
 
 app.use('/users', useersRouter);
