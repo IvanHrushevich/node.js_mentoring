8cf73b5b7d2aa80bf20300af906503c538571f48
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2019-12-21 17:07:31 +0300

    Add get by id route

--- src/hw2/app.js
+++ src/hw2/app.js
@@ -3,12 +3,27 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-const users = {};
+const users = {
+  1: {
+    id: '1',
+    login: 'ivan_hrushevich',
+    password: 'asdasd1!',
+    age: 17,
+    isDeleted: false
+  }
+};
 
 app.listen(port, () => console.log(`Users app listening on port ${port}!`));
 
 const useersRouter = express.Router();
 useersRouter.use(express.json());
+
+useersRouter.get('/:id', (req, res) => {
+  const id = req.params.id;
+  const user = users[id];
+
+  return user ? res.json(user) : res.sendStatus(404);
+});
 
 useersRouter.get('/', (req, res) => res.json(users));
 
