14037c1dc859e2519bcdc654d4812f727d8732eb
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2019-12-22 17:59:08 +0300

    refactoring: move route and controllers to separate folders

---
+++ src/hw2/controllers/users.js
@@ -0,0 +1,97 @@
+import uuid from 'uuid/v1';
+
+import { schema, errorResponse } from '../validation/index';
+
+const users = {
+  1: {
+    id: '1',
+    login: 'ivan_hrushevich',
+    password: 'asdasd1!',
+    age: 17,
+    isDeleted: false
+  }
+};
+
+export const getFilter = (req, res) => {
+  const searchStr = req.query.login;
+  const limit = req.query.limit;
+
+  const filteredUserList = getAutoSuggestUsers(searchStr, limit);
+
+  if (filteredUserList.length) {
+    const filteredUsers = filteredUserList.reduce((acc, curr) => {
+      acc[curr.id] = curr;
+      return acc;
+    }, {});
+
+    return res.json(filteredUsers);
+  } else {
+    return res.sendStatus(404);
+  }
+};
+
+export const getById = (req, res) => {
+  const id = req.params.id;
+  const user = users[id];
+
+  return user && user.isDeleted === false ? res.json(user) : res.sendStatus(404);
+};
+
+export const getUsers = (req, res) => res.json(users);
+
+export const postUser = (req, res) => {
+  const user = req.body;
+
+  const { error } = schema.validate(user, {
+    abortEarly: false,
+    allowUnknown: false
+  });
+
+  if (error) {
+    return res.status(400).json(errorResponse(error.details));
+  } else {
+    const id = uuid();
+    user.id = id;
+    user.isDeleted = false;
+    users[user.id] = user;
+
+    return res.sendStatus(200);
+  }
+};
+
+export const putUserById = (req, res) => {
+  const id = req.params.id;
+  let user = users[id];
+  const updatedUser = req.body;
+
+  if (user && user.isDeleted === false) {
+    users[id] = {
+      ...user,
+      ...updatedUser
+    };
+
+    return res.sendStatus(200);
+  } else {
+    return res.sendStatus(404);
+  }
+};
+
+export const deleteUserById = (req, res) => {
+  const id = req.params.id;
+  const user = users[id];
+
+  if (user && user.isDeleted === false) {
+    user.isDeleted = true;
+
+    return res.sendStatus(200);
+  } else {
+    return res.sendStatus(404);
+  }
+};
+
+const getAutoSuggestUsers = (searchStr, limit) => {
+  const userList = Object.values(users);
+  const filteredUserList = userList.filter(user => user.login.includes(searchStr)).slice(0, limit);
+
+  return filteredUserList;
+};
---
+++ src/hw2/routes/users.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import * as usersControllers from '../controllers/users';
+
+const usersRouter = express.Router();
+usersRouter.use(express.json());
+
+// request example: http://localhost:3000/users/filter?login=iv&limit=5
+usersRouter.get('/filter', usersControllers.getFilter);
+usersRouter.get('/:id', usersControllers.getById);
+usersRouter.get('/', usersControllers.getUsers);
+
+usersRouter.post('/', usersControllers.postUser);
+
+usersRouter.put('/:id', usersControllers.putUserById);
+
+usersRouter.delete('/:id', usersControllers.deleteUserById);
+
+export default usersRouter;
