8e4349cd8ec473111d7b681cb0fe0a84f18e261a
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-03-08 12:44:49 +0300

    feat: add CORS middleware

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import express from 'express';
+import cors, { CorsOptions } from 'cors';
 
 import { usersRouter, groupsRouter } from './controllers/index';
 import { requestLogger, logger } from './logging/index';
 import { authentication, errorHandler, checkToken } from './middlewares/index';
 
+const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE'],
+    optionsSuccessStatus: 200
+};
+
 const app: express.Express = express();
 const port: string = process.env.PORT || '3000';
 
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(requestLogger);
