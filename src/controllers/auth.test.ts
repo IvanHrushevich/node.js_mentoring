eb7fa5da853fe8c25122a90022668d25c25fe0d6
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-03-23 23:43:21 +0300

    fix: update .babelrc

--- src/controllers/auth.test.ts
+++ src/controllers/auth.test.ts
@@ -2,6 +2,7 @@ import { authRouter } from './auth';
 
 describe('Auth controller', () => {
     test('adds 1 + 2 to equal 3', () => {
+        console.log('authRouter', authRouter);
         expect(1 + 2).toBe(3);
     });
 });
