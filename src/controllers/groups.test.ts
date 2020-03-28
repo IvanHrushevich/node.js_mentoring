2de968aaa24038caee4d80cb0272948f8c79d338
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-03-29 01:57:07 +0300

    test: add tests for groupsController

--- src/controllers/groups.test.ts
+++ src/controllers/groups.test.ts
@@ -6,13 +6,19 @@ jest.mock('../data-access/group.dao');
 import { groupsController } from './groups';
 import { getMockRes, fakeGroup } from '../spec-utils/index';
 import { Group } from '../interfaces/group.model';
+import { SeqUpdateResponse } from '../interfaces/seq-update-response';
 
 describe('Groups controller', () => {
+    const mockRes = getMockRes();
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     test('should call res.json with fetched group on getById call', async () => {
         const mockReq = {
             params: { id: 'fakeId' }
         } as any;
-        const mockRes = getMockRes();
 
         await groupsController.getById(mockReq, mockRes);
 
@@ -21,7 +27,6 @@ describe('Groups controller', () => {
 
     test('should call res.json with array of fetched groups on groupsController call', async () => {
         const mockReq = {} as any;
-        const mockRes = getMockRes();
 
         await groupsController.getAllGroups(mockReq, mockRes);
 
@@ -40,11 +45,46 @@ describe('Groups controller', () => {
         const mockReq = {
             body: group
         } as any;
-        const mockRes = getMockRes();
 
         await groupsController.postGroup(mockReq, mockRes);
 
         expect(mockRes.status).toBeCalledWith(201);
         expect(mockRes.json).toBeCalledWith(expectedGroup);
+    });
+
+    test('should call res.status with 200 and res.json with true on successful postAddUsersToGroup call', async () => {
+        const mockReq = {
+            params: { id: 'fakeGroupId' },
+            body: ['fakeUserId1', 'fakeUserId2']
+        } as any;
+        const expectedResult: boolean = true;
+
+        await groupsController.postAddUsersToGroup(mockReq, mockRes);
+
+        expect(mockRes.status).toBeCalledWith(200);
+        expect(mockRes.json).toBeCalledWith(expectedResult);
+    });
+
+    test('should call res.status with 200 and res.json with proper response on successful putGroupById call', async () => {
+        const mockReq = {
+            params: { id: 'fakeGroupId' },
+            body: fakeGroup
+        } as any;
+        const expectedResult: SeqUpdateResponse<Group> = [1, [fakeGroup]];
+
+        await groupsController.putGroupById(mockReq, mockRes);
+
+        expect(mockRes.status).toBeCalledWith(200);
+        expect(mockRes.json).toBeCalledWith(expectedResult);
+    });
+
+    test('should call res.status with 200 on successful deleteGroupById call', async () => {
+        const mockReq = {
+            params: { id: 'fakeGroupId' }
+        } as any;
+
+        await groupsController.deleteGroupById(mockReq, mockRes);
+
+        expect(mockRes.status).toBeCalledWith(200);
     });
 });
