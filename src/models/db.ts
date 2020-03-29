eb6f7ef37b72b08aed31787082db3e82fd268935
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-03-30 00:00:37 +0300

    refactor: move connection string to .env file

--- src/models/db.ts
+++ src/models/db.ts
@@ -1,7 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const connectionURI: string =
-    'postgres://agzctnieexodcz:bda6e1e7a30c12426c357809944e754486e23d4ed40c473146051e078c875e15@ec2-54-195-252-243.eu-west-1.compute.amazonaws.com:5432/dfpm81bm9safgr';
+const connectionURI: string = process.env.DB_CONNECTION_URI as string;
 
 export const db: Sequelize = new Sequelize(connectionURI, {
     dialect: 'postgres',
